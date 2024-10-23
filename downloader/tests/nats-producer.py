4f45b691a692e8981d3b1ea54eff4acc1473b3a8
Daniele <daniele.sartiano@gmail.com>
2024-10-23 22:20:42 +0200

    Update libs

--- downloader/tests/nats-producer.py
+++ downloader/tests/nats-producer.py
@@ -9,7 +9,7 @@ from nats.aio.client import Client as NATS
 from nats.aio.errors import ErrConnectionClosed, ErrTimeout, ErrNoServers
 
 
-async def run(loop):
+async def main():
 
     nc = NATS()
 
@@ -22,8 +22,7 @@ async def run(loop):
     await nc.connect("nats://ruser:T0pS3cr3t@127.0.0.1:4222",
                      reconnected_cb=reconnected_cb,
                      disconnected_cb=disconnected_cb,
-                     max_reconnect_attempts=-1,
-                     loop=loop)
+                     max_reconnect_attempts=-1)
 
     #for url in ["www.corriere.it", "www.repubblica.it", "www.sartiano.info", "www.unipi.it"]:
     for urls in [["https://www.unipi.it", "https://www.sartiano.info", "https://www.corriere.it", "https://www.repubblica.it", "https://www.github.com", "https://news.google.com"]]:
@@ -38,5 +37,5 @@ async def run(loop):
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run(loop))
+    loop.run_until_complete(main())
     loop.close()
